b9bdae534f9d9e10fc7515f9bec0635d8e6368e3
llkui <liukui178@163.com>
2017-11-15 16:24:22 +0800

    页面布局优化

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'app-admin',
-	template: '<router-outlet></router-outlet>',
+	template: `
+		<ng-container *ngIf="layout == 'left-right'">
+			<div class="container flex">
+				<left-nav></left-nav>
+				<div class="right-content flex-1">
+					<router-outlet></router-outlet>
+				</div>
+			</div>
+		</ng-container>
+		<ng-container *ngIf="layout == 'all'">
+			<router-outlet></router-outlet>
+		</ng-container>
+	`,
 })
 export class AdminComponent{
-	
+	layout = 'left-right';
+
+	constructor(private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		this.route.queryParams.subscribe((params) => {
+			if(params.layout){
+				this.layout = params.layout;
+			}
+		});
+	}
 }
