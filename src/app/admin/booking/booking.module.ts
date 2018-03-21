b20cbf15ab80c3aaeb34e3b678798ecfc5be09a3
llkui <liukui178@163.com>
2018-03-21 14:10:22 +0800

    booking重复模块注释

--- src/app/admin/booking/booking.module.ts
+++ src/app/admin/booking/booking.module.ts
@@ -1,34 +1,34 @@
-import { NgModule }                  from '@angular/core';
-import { CommonModule }              from '@angular/common';
-import { FormsModule }               from '@angular/forms';
-
-import { NavModule }                 from '../nav/nav.module';
-
-import { PipeModule }                from '../../pipe/pipe.module';
-
-import { AngCommonModule }           from '../../common/ang-common.module';
-
-import { BookingRoutingModule }      from './booking.routing.module';
-
-import { BookingComponent }          from './booking.component';
-
-@NgModule({
-    declarations: [
-        BookingComponent,
-    ],
-    exports: [
-        BookingComponent,
-    ],
-    imports: [
-        CommonModule,
-        FormsModule,
-        NavModule,
-        PipeModule,
-        AngCommonModule,
-        BookingRoutingModule,
-    ]
-})
-
-export class BookingModule{
-
-}
+// import { NgModule }                  from '@angular/core';
+// import { CommonModule }              from '@angular/common';
+// import { FormsModule }               from '@angular/forms';
+//
+// import { NavModule }                 from '../nav/nav.module';
+//
+// import { PipeModule }                from '../../pipe/pipe.module';
+//
+// import { AngCommonModule }           from '../../common/ang-common.module';
+//
+// import { BookingRoutingModule }      from './booking.routing.module';
+//
+// import { BookingComponent }          from './booking.component';
+//
+// @NgModule({
+//     declarations: [
+//         BookingComponent,
+//     ],
+//     exports: [
+//         BookingComponent,
+//     ],
+//     imports: [
+//         CommonModule,
+//         FormsModule,
+//         NavModule,
+//         PipeModule,
+//         AngCommonModule,
+//         BookingRoutingModule,
+//     ]
+// })
+//
+// export class BookingModule{
+//
+// }
