738107275ac3727ec5559eb843b83e48f5cee126
llkui <liukui178@163.com>
2018-05-09 10:12:12 +0800

    登录、忘记密码优化

--- src/app/admin/login/forgetpwd.component.ts
+++ src/app/admin/login/forgetpwd.component.ts
@@ -1,9 +1,17 @@
-import { Component }                          from '@angular/core';
-import { Router }                             from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+    FormBuilder,
+    FormGroup,
+    FormControl,
+    Validators,
+} from '@angular/forms';
 
-import { AuthService }                        from '../../auth.service';
-import { AdminService }                       from '../admin.service';
-import { config }                             from '../../config';
+import { NzMessageService } from 'ng-zorro-antd';
+
+import { AuthService } from '../../auth.service';
+import { AdminService } from '../admin.service';
+import { config } from '../../config';
 
 @Component({
     selector: 'forgetpwd',
@@ -12,51 +20,42 @@ import { config }                             from '../../config';
 })
 
 export class ForgetpwdComponent{
-	toast: {
-		show: number,
-		text: string,
-		type:  string,
-	};
     num: number;
     codeImg = config.baseHTTP + '/mebcrm/getcode?id=1';
     sms: {
         text: string,
         disabled: boolean,
     }
-    info: {
-        username: string,
-        vali_code: string,
-        verify_code: string,
-        password: string,
-    }
-    btnCanEdit: boolean;
+    isLoadingSave: boolean;
+    validateForm: FormGroup;
 
 	constructor(
+        private fb: FormBuilder,
+        private _message: NzMessageService,
 		public authService: AuthService,
 		public adminService: AdminService,
 		public router: Router,
-	) {}
+	) {
+        this.validateForm = this.fb.group({
+            username: [ '', [ Validators.required ]],
+            password: [ '', [ Validators.required ]],
+            vali_code: [ '', [ Validators.required ]],
+            verify_code: [ '', [ Validators.required ]],
+        });
+    }
 
 	ngOnInit(): void {
-		this.toast = {
-			show: 0,
-			text: '',
-			type: '',
-		};
-
         this.num = 1;
         this.sms = {
             text: '验证码',
             disabled: false,
         }
-        this.info = {
-            username: '',
-            vali_code: '',
-            verify_code: '',
-            password: '',
-        }
 
-        this.btnCanEdit = false;
+        this.isLoadingSave = false;
+    }
+
+    getFormControl(name) {
+        return this.validateForm.controls[ name ];
     }
 
     changeCodeImg() {
@@ -68,16 +67,16 @@ export class ForgetpwdComponent{
     sendSms() {
         this.sms.disabled = true;
         var params = {
-			mobile: this.info.username,
+			mobile: this.validateForm.controls.username.value,
 			action_type: 101,
-			vali_code: this.info.vali_code,
+			vali_code: this.validateForm.controls.vali_code.value,
         }
         this.authService.smsverifycode(params).then((data) => {
             if(data.status == 'no'){
-                this.toastTab(data.errorMsg, 'error');
+                this._message.error(data.errorMsg);
                 this.sms.disabled = false;
             }else{
-                this.toastTab('短信验证码发送成功', '');
+                this._message.success('短信验证码发送成功');
                 var count = 60;
                 this.sms.text = count + '秒后重发';
                 var verifyCodeInterval = setInterval(() => {
@@ -90,56 +89,61 @@ export class ForgetpwdComponent{
                 }, 1000);
             }
         }).catch(() => {
-            this.toastTab('服务器错误', 'error');
+            this._message.error('服务器错误');
             this.sms.disabled = false;
         });
     }
 
     updatePwd() {
-        this.btnCanEdit = true;
-        if(!this.info.password.match(/^\w+$/)){
-            this.toastTab('新密码只能由字母、数字和下划线组成', 'error');
-            this.btnCanEdit = false;
+        this.isLoadingSave = true;
+        if(this.validateForm.controls.username.value.length != 11){
+            this._message.error('手机号码不足11位');
+            this.isLoadingSave = false;
+            return;
+        }
+        if(this.validateForm.controls.vali_code.value.length != 4){
+            this._message.error('图形验证码不足4位');
+            this.isLoadingSave = false;
+            return;
+        }
+        if(this.validateForm.controls.verify_code.value.length == 0){
+            this._message.error('验证码不可为空');
+            this.isLoadingSave = false;
+            return;
+        }
+        if(this.validateForm.controls.password.value.length == 0){
+            this._message.error('新密码不可为空');
+            this.isLoadingSave = false;
+            return;
+        }
+        if(!this.validateForm.controls.password.value.match(/^\w+$/)){
+            this._message.error('新密码只能由字母、数字和下划线组成');
+            this.isLoadingSave = false;
             return;
         }
         var params = {
-            mobile: this.info.username,
-            verify_code: this.info.verify_code,
-            password: this.info.password,
+            mobile: this.validateForm.controls.username.value,
+            verify_code: this.validateForm.controls.verify_code.value,
+            password: this.validateForm.controls.password.value,
         }
 
         this.authService.forgetpwd(params).then((data) => {
             if(data.status == 'no'){
-                this.toastTab(data.errorMsg, 'error');
-                this.btnCanEdit = false;
+                this._message.error(data.errorMsg);
+                this.isLoadingSave = false;
             }else{
-                this.toastTab('密码修改成功', '');
+                this._message.success('密码修改成功');
                 setTimeout(() => {
                     this.router.navigate(['./login']);
                 }, 2000);
             }
         }).catch(() => {
-            this.toastTab('服务器错误', 'error');
-            this.btnCanEdit = false;
+            this._message.error('服务器错误');
+            this.isLoadingSave = false;
         });
     }
 
     login() {
         this.router.navigate(['./login']);
     }
-
-	toastTab(text, type) {
-		this.toast = {
-			show: 1,
-			text: text,
-			type: type,
-		}
-		setTimeout(() => {
-	    	this.toast = {
-				show: 0,
-				text: '',
-				type: '',
-			}
-	    }, 2000);
-	}
 }
